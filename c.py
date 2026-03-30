068fb53b219bfe8421ecbb93eda66513070c190f
Yathesh <yatheshinfo@gmail.com>
2026-03-30 11:24:40 +0530

    deleted c.py

--- c.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-ec2 = boto3.client('ec2', region_name='ap-southeast-1')
-
-response = ec2.terminate_instances(
-    InstanceIds=['i-05048bd5635472385']  # replace with your instance ID
-)
-
-print("Terminating:", response['TerminatingInstances'])
